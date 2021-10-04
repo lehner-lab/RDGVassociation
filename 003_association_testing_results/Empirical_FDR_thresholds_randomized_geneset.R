75a3f1137d7ffada9daa1fff51af654b1bccaccc
mischanvp <mischan.vali@gmail.com>
2021-10-04 11:09:46 +0200

    folders and first scripts

---
+++ 003_association_testing_results/Empirical_FDR_thresholds_randomized_geneset.R
@@ -0,0 +1,97 @@
+---
+  title: "Upload the data from all models and set FDR thresholds"
+---
+  
+#rm(list=ls())
+
+library(tidyverse)
+library(dplyr)
+
+
+###run code inside /002_association_testing_results/ folder
+
+##directories
+input_file_direc='./input_files/'
+output_file_direc='./results/'
+
+##upload models to be discarded, extracted via lambdas.R script
+models_discard <- read.csv(file = paste(output_file_direc,'models_removed_based_on_lambda_randomized_geneset.txt',sep=''),head=TRUE,sep ='\t',stringsAsFactors=FALSE) 
+
+
+####### estimation of empirical FDRs for TCGA-discovery
+##upload SKAT-O results from randomization
+#remove models which will be removed based on inflation factor
+SKAT_res <- read.csv(file= paste(input_file_direc,'Discovery_TCGA_SKATO_random_randomized_geneset.txt',sep=''),head=TRUE,sep ='\t',stringsAsFactors=FALSE) %>%
+  dplyr::filter(!model_snps_cancer_pheno %in% models_discard$model_snps_cancer_pheno)
+
+
+##make plot of the p value distributions of all combined
+p_values_random <- SKAT_res %>%
+  group_by(cancer_type) %>%
+  mutate(LOG10pValue=-log10(pValue)) %>%
+  mutate(rankP=rank(-LOG10pValue)) %>%
+  ungroup()  
+
+##save the FDR information
+FDR_calculated <- c()
+
+for(i in unique(p_values_random$cancer_type)){
+  rm(p_values_random_cancer_type)
+  rm(FDR_cancer_type) 
+  
+  p_values_random_cancer_type <- p_values_random %>%
+    dplyr::filter(cancer_type==i)
+  
+  FDR_cancer_type <- data.frame(FDR= seq(0,1,0.01),
+                                FDR_threshold= quantile(p_values_random_cancer_type$pValue, seq(0,1,0.01)),
+                                stringsAsFactors = F)
+  FDR_cancer_type$cancer_type <- i
+  
+  FDR_calculated <- FDR_calculated %>%
+    rbind(FDR_cancer_type)
+}
+
+write.table(FDR_calculated,
+            file= paste(output_file_direc,'TCGA_empirical_FDRs_random_geneset.txt',sep=''),
+            quote=FALSE, sep='\t',row.names=FALSE,col.names = T)
+
+
+
+####### estimation of empirical FDRs for PCAWG_Hartwig-validation
+##upload SKAT-O results from randomization
+#remove models which will be removed based on inflation factor
+SKAT_res <- read.csv(file= paste(input_file_direc,'Validation_PCAWG_Hartwig_SKATO_random_randomized_geneset.txt',sep=''),head=TRUE,sep ='\t',stringsAsFactors=FALSE) %>%
+  dplyr::filter(!model_snps_cancer_pheno %in% models_discard$model_snps_cancer_pheno)
+
+
+##make plot of the p value distributions of all combined
+p_values_random <- SKAT_res %>%
+  group_by(cancer_type) %>%
+  mutate(LOG10pValue=-log10(pValue)) %>%
+  mutate(rankP=rank(-LOG10pValue)) %>%
+  ungroup()  
+
+##save the FDR information
+FDR_calculated <- c()
+
+for(i in unique(p_values_random$cancer_type)){
+  rm(p_values_random_cancer_type)
+  rm(FDR_cancer_type) 
+  
+  p_values_random_cancer_type <- p_values_random %>%
+    dplyr::filter(cancer_type==i)
+  
+  FDR_cancer_type <- data.frame(FDR= seq(0,1,0.01),
+                                FDR_threshold= quantile(p_values_random_cancer_type$pValue, seq(0,1,0.01)),
+                                stringsAsFactors = F)
+  FDR_cancer_type$cancer_type <- i
+  
+  FDR_calculated <- FDR_calculated %>%
+    rbind(FDR_cancer_type)
+}
+
+write.table(FDR_calculated,
+            file= paste(output_file_direc,'PCAWG_Hartwig_empirical_FDRs_random_geneset.txt',sep=''),
+            quote=FALSE, sep='\t',row.names=FALSE,col.names = T)
+
+
---
+++ 003_association_testing_results/lambdas_randomized_geneset.R
@@ -0,0 +1,120 @@
+---
+  title: "Estimation of inflation factors for randomized geneset"
+---
+  
+#rm(list=ls())
+  
+library(tidyverse)
+library(dplyr)
+library(ggrastr)
+
+
+###run code inside /002_association_testing_results/ folder
+
+##directories
+input_file='./input_files/Discovery_TCGA_SKATO_results_randomized_geneset.txt'
+output_file_direc='./results/'
+output_figure_direc='./figures/'
+
+table_name='SKAT_models'
+
+
+##upload SKAT-O results
+SKAT_res <- read.csv(file =input_file,head=TRUE,sep ='\t',stringsAsFactors=FALSE) 
+
+##calculate lambdas
+lambdas_results <- c()
+
+for(i in unique(SKAT_res$model_snps_cancer_pheno)){
+  print(i)
+  rm(Inflation_pvalues)
+  rm(Inflation_chisq)
+  rm(lambda)
+  rm(assc_test)
+  
+  assc_test <- SKAT_res %>%
+    dplyr::filter(model_snps_cancer_pheno == rlang::sym(i))
+  
+  ##estimate genomic inflation factor lambda 
+  Inflation_pvalues <- assc_test$pValue
+  Inflation_chisq <- qchisq(1-Inflation_pvalues,1)
+  lambda = round(median(Inflation_chisq)/qchisq(0.5,1),2)
+  
+  ##save lambda values in table
+  rm(lambda_values)
+  lambda_values <- data.frame(model_snps_cancer_pheno=i,
+                              model_snps=unique(assc_test$model_snps),
+                              model=unique(assc_test$model),
+                              snps=unique(assc_test$snps),
+                              cancer_type=unique(assc_test$cancer_type),
+                              pheno=unique(assc_test$pheno),
+                              lambda_qq=lambda,
+                              n_genes=nrow(assc_test),
+                              n_samples=unique(assc_test$n_samples),
+                              stringsAsFactors = F)
+  
+  lambdas_results <- lambdas_results %>%
+    rbind(lambda_values)
+}#end looping through cancer pheno pairs
+
+
+###plotting lambdas
+lambdas_results_filtered <- lambdas_results %>%
+  mutate(snps= factor(snps, levels =c('PTV','PTV_Misssense_CADD25','PTV_Misssense_CADD15','Misssense_MTR','Misssense_CCR'))) %>%
+  mutate(cancer_type=sub('Brain_glioma_multi','Brain\nglioma\nmulti',cancer_type)) %>%
+  mutate(cancer_type=sub('Brain_glioma_low','Brain\nglioma\nlow',cancer_type))  %>%
+  mutate(cancer_type=sub('Stomach_Eso','Stomach\nEso',cancer_type))  %>%
+  mutate(cancer_type=sub('Colon_Rectum','Colon\nRectum',cancer_type)) %>%
+  dplyr::filter(n_genes >= 100) %>% #only lambdas where at least 100 genes were tested
+  mutate(removed=if_else(lambda_qq >= 1.5, 'discarded','kept')) #inflation factors bigger or equal to 1.5 should be marked
+  
+pdf(file= paste(output_figure_direc,table_name,'_lambdas_distribution_VAE_ICA_randomized_geneset','.pdf',sep=''),
+    width= 8,
+    height =10)  
+ggplot(lambdas_results_filtered, 
+       aes(x=snps,y=lambda_qq, label=pheno, fill=n_genes)) +
+  geom_jitter_rast(alpha=0.4, 
+                   size=1,
+                   height=0,
+                   width=0.2,
+                   aes(x=snps,
+                       y=lambda_qq,
+                       color=removed),
+                   lambdas_results_filtered) + 
+  geom_boxplot(outlier.shape = NA,
+               color='black') + 
+  scale_fill_gradientn(colours = rainbow(5)) +
+  theme_bw() +
+  theme(panel.grid.minor = element_blank()) +
+  theme(axis.text.x = element_text(size=8, angle = 70,hjust=1, color='black'),
+        axis.ticks.x=element_blank(),
+        axis.text.y = element_text(size=8, color='black'),
+        axis.title = element_text(size=10, color='black'),
+        strip.text.x = element_text(size=10, color='black'),
+        strip.text.y = element_text(size=8, color='black'),
+        legend.position = "right",
+        legend.text = element_text(size=8)) +
+  facet_grid(cancer_type ~ model) +
+  xlab('') +
+  ylab('inflation factor') +
+  labs(fill='n genes tested') +
+  labs(color='discarded?') +
+  scale_colour_manual(values=c("discarded" = "#F21A00","kept" = "#3B9AB2"))
+dev.off()
+
+
+####write out the ones which will be removed
+models_removed <- lambdas_results_filtered %>%
+  dplyr::filter(removed == 'discarded') %>%
+  mutate(cancer_type=sub('Brain\nglioma\nmulti','Brain_glioma_multi',cancer_type)) %>%
+  mutate(cancer_type=sub('Brain\nglioma\nlow','Brain_glioma_low',cancer_type))  %>%
+  mutate(cancer_type=sub('Stomach\nEso','Stomach_Eso',cancer_type))  %>%
+  mutate(cancer_type=sub('Colon\nRectum','Colon_Rectum',cancer_type))
+
+write.table(models_removed,
+            file= paste(output_file_direc,'models_removed_based_on_lambda_randomized_geneset.txt',sep=''),
+            quote=FALSE, sep='\t',row.names=FALSE,col.names = T)
+
+
+
+
